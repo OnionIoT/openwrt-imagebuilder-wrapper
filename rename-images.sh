0d01b20e1b7e8bfbb685559c48fe0587ea00ffa4
Lazar Demin <lazar@onion.io>
2022-11-22 20:20:59 +0000

    changed script that renames output images - new naming format

--- rename-images.sh
+++ rename-images.sh
@@ -2,15 +2,21 @@
 
 ### rename firmware images based on device name and version number
 
+OPENWRT_VERSION="22.03.2"
+TARGET="ramips"
+SUBTARGET="mt76x8"
 
-DIR="openwrt-imagebuilder/bin/targets/ramips/mt76x8"
+DIR="openwrt-imagebuilder-$OPENWRT_VERSION/bin/targets/$TARGET/$SUBTARGET"
 
-## find version and build number
-# get repo location
-PKG_URL=$(cat config/new-repositories.conf | sed -e 's/.*http/http/' -e 's/$/\/Packages/')
-# grab package info file, find version number of omega2-base package, format properly
-VERSION_INFO=$(wget -q -O - $PKG_URL | grep -A1 "Package: omega2-base$" | grep Version | sed -e 's/Version: //' -e 's/^/v/' -e 's/-/-b/')
+# Disabling this method for now
+# ## find version and build number
+# # get repo location
+# PKG_URL=$(cat config/new-repositories.conf | sed -e 's/.*http/http/' -e 's/$/\/Packages/')
+# # grab package info file, find version number of omega2-base package, format properly
+# VERSION_INFO=$(wget -q -O - $PKG_URL | grep -A1 "Package: omega2-base$" | grep Version | sed -e 's/Version: //' -e 's/^/v/' -e 's/-/-b/')
 
+DATE=$(date +"%Y%m%d")
+VERSION_INFO="${OPENWRT_VERSION}-${DATE}"
 
 renameImage () {
     device="$1"
@@ -20,7 +26,7 @@ renameImage () {
     fi
 
     imageName="${device}-${VERSION_INFO}.bin"
-    cp $DIR/openwrt-21.02.1-ramips-mt76x8-${device}-squashfs-sysupgrade.bin output/$imageName
+    cp $DIR/openwrt-${OPENWRT_VERSION}-${TARGET}-${SUBTARGET}-${device}-squashfs-sysupgrade.bin output/$imageName
 }
 
 # rename images
